26bfe2980fbb2ba3f0de9d1b1e1917e3dc680279
heagenb03 <heagenbell@outlook.com>
2024-09-23 21:41:09 -0500

    modified:   __pycache__/app.cpython-312.pyc 	new file:   __pycache__/forms.cpython-312.pyc 	modified:   app.py 	modified:   requirements.txt 	new file:   static/assets/favicon-png.png 	new file:   static/assets/fox-algorithm.jpg 	modified:   static/assets/github-img.png 	new file:   static/assets/minecraft-modification.jpg 	new file:   static/assets/resume.pdf 	modified:   static/styles/styles.css 	modified:   templates/index.html 	new file:   templates/interal_server_error.html

--- app.py
+++ app.py
@@ -1,57 +1,61 @@
-import os
-import resend
 from flask import Flask, render_template, redirect, request
+from flask_mail import Mail, Message
+import os
 
 app = Flask(__name__,template_folder='./templates', static_folder='./static')
 
-resend.api_key = os.environ["RESEND_API_KEY"]
-ADMIN_EMAIL = os.environ["ADMIN_EMAIL"]
-CONTACT_EMAIL = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New Message Notification</title>
-</head>
-<body>
-    <h2>New Message Received!</h2>
-    <p><strong>Name:</strong> {name}</p>
-    <p><strong>Email:</strong> {email}</p>
-    <p><strong>Message:</strong></p>
-    <blockquote>
-        {message}
-    </blockquote>
-</body>
-</html>
-"""
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+app.config['MAIL_USERNAME'] = 'contact@heagenbell.org'
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
+mail = Mail()
+mail.init_app(app)
+
+def sendContactForm(result):
+    msg = Message(
+                  subject="Contact Form from Portfolio Website",
+                  sender="contact@heagenbell.org",
+                  recipients=["bellheagen@gmail.com"])
+    
+    msg.body = """
+    
+    Contact form received from Portfolio Website.
+    
+    Name: {}
+    Email: {}
+    Message: {}
+    
+    --End of Message--
+    """.format(result['name'], result['email'], result['message'])
+    
+    mail.send(msg)
+    
+#Flask Setup
 @app.route("/")
 def main():
     return render_template("index.html")
 
-@app.route("/contact", methods=["GET", "POST"])
+@app.route("/", methods=["POST"])
 def contact():
     if request.method == "POST":
-        # Load form data
-        form_data = request.form.to_dict()
+        result = {}
+        
+        result['name'] = request.form["name"]
+        result['email'] = request.form["email"].replace(" ", "").lower()
+        result['message'] = request.form["message"]
 
-        # Configuring the email fields
-        params = {
-            "from": "Your Flask App <delivered@resend.dev>",
-            "to": [ADMIN_EMAIL],
-            "subject": f"New message from {form_data['name']}!",
-            "html": CONTACT_EMAIL.format(**form_data),
-        }
-
-        # Sending the email and catching response
-        response = resend.Emails.send(params)
-
-        # Handle the response
-        if response.get("id"):
-            return redirect("/contact")
-        else:
-            return {"message": "Something went wrong. Please try again."}
-    else:
-        # Render the contact form
+        sendContactForm(result)
+        
         return render_template("index.html")
+    
+    return render_template("index.html")
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template("internal_server_error.html"), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
